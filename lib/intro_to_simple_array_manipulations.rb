bc0545fbdfaafd99982293bfe04976edf8a9dc9d
Andrew J Casarsa <acasarsa@gmail.com>
2020-01-31 15:57:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_push(arr, el)
 end
 
 def using_unshift(arr, el)
-  unshifted_array = arr.unshift(el)
+  unshifted_array = arr.unshift(el)  #+> these seem to return a new array and take default variable of the method name
 end
 
 def using_pop(arr)
@@ -22,6 +22,10 @@ def shift_with_args(arr)
   arr.shift(2)
 end
 
-def using_concat(arr, otherArr) #=> takes in 2 arrays, and adds the otherArr to the first Arr
-  arr.concat(otherArr)
+def using_concat(arr, otherArr) # takes in 2 arrays, and adds the otherArr to the first Arr
+  arr.concat(otherArr) #=> returns combined arrays
+end
+
+def using_insert(arr, el)
+  arr.insert(4, el)  # at index [4] insert element into arr
 end
