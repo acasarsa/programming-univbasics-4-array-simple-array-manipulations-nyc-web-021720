b0eb8827fb2ba3f787e5a45daa36ef47458db1b5
Andrew J Casarsa <acasarsa@gmail.com>
2020-01-31 16:01:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(arr, otherArr) # takes in 2 arrays, and adds the otherArr to th
 end
 
 def using_insert(arr, el)
-  arr.insert(4, el)  # at index [i] insert element into arr
+  arr.insert(i, el)  # at index [i] insert element into arr
 end
 
 def using_uniq(arr)
